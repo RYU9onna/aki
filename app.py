36a2d9adc7d0e6583731f741b6cb95bef330d155
RYU9onna <129857611+RYU9onna@users.noreply.github.com>
2023-05-12 08:34:08 +0900

    Update app.py

--- app.py
+++ app.py
@@ -18,7 +18,7 @@ def home():
     if request.method == 'POST':
         if "play" in request.form:
             # GPT-3にトピックを考えさせる
-            chat = openai.ChatCompletion.create(model="gpt-4", messages=[{"role": "system", "content": "あなたは私の20問ゲームの対戦相手です。今から1つの道具、家電、物体、食べ物、生き物、日本の有名人などから名前だけを考えてください。その名前だけ返事してください。"}, {"role": "user", "content": "名前を１つ決めてください。その名前だけ返事してください。"}], temperature=1.0)
+            chat = openai.ChatCompletion.create(model="gpt-4", messages=[{"role": "system", "content": "あなたは私の20問ゲームの対戦相手です。今から1つの道具、家電、物体、食べ物、生き物、日本の芸能人などから名前だけを考えてください。その名前だけ返事してください。"}, {"role": "user", "content": "名前を１つ決めてください。その名前だけ返事してください。"}], temperature=2.0)
             session['topic'] = chat['choices'][0]['message']['content']
             return render_template('index.html', message="私が考えているのは何でしょう？質問して当ててみて")
         elif "surrender" in request.form:
