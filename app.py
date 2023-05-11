542b8300c6219c5f5a29ca1f25208cf6d5a63bc7
RYU9onna <129857611+RYU9onna@users.noreply.github.com>
2023-05-12 08:24:17 +0900

    Update app.py

--- app.py
+++ app.py
@@ -34,8 +34,8 @@ def home():
                     return render_template('index.html', message=f"正解です！答えは {session['topic']} でした！", answer=session['topic'])
                 # GPT-4に質問を評価させる
                 chat = openai.ChatCompletion.create(model="gpt-3.5-turbo-0301", messages=[
-                    {"role": "system", "content": "あなたは私の20問ゲームの対戦相手です。はい、いいえ、などの一言で簡単な返事をします。答えは言いません。"},
-                    {"role": "user", "content": f'"「{session["topic"]}"は、"{question}"？」と質問します。どうですか？'}
+                    {"role": "system", "content": "あなたは私の20問ゲームの対戦相手です。"},
+                    {"role": "user", "content": f'"「{session["topic"]}"は、"{question}"？」と質問します。「はい」「少しそう」「どちらでもない」「違います」「少し違う」のいずれかだけで返事をします。'}
                 ])
                 answer = chat['choices'][0]['message']['content']
                 return render_template('index.html', message=answer)
