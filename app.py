0b46f7a3590647495597245b81e989eaf5f0830e
RYU9onna <129857611+RYU9onna@users.noreply.github.com>
2023-05-11 21:38:11 +0900

    Update app.py

--- app.py
+++ app.py
@@ -18,7 +18,7 @@ def home():
     if request.method == 'POST':
         if "play" in request.form:
             # GPT-3にトピックを考えさせる
-            chat = openai.ChatCompletion.create(model="gpt-3.5-turbo", messages=[{"role": "system", "content": "あなたは親切なアシスタントです。"}, {"role": "user", "content": "20問ゲームのためのトピックを考えてください。"}])
+            chat = openai.ChatCompletion.create(model="gpt-3.5-turbo-0301", messages=[{"role": "system", "content": "あなたは親切なアシスタントです。ユーザーが20問ゲームをプレイするための具体的な1つのトピックを考えてください。"}, {"role": "user", "content": "20問ゲームのためのトピックを考えてください。"}])
             session['topic'] = chat['choices'][0]['message']['content']
             return render_template('index.html', message="私が考えているのは何でしょう？質問して当ててみて")
         elif "surrender" in request.form:
